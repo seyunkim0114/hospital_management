63cbc9b4569044b408fc31945219c5074792a79d
seyunkim0114 <seyun0114kim@gmail.com>
2022-11-30 22:03:18 -0500

    get all patients by nurseid

--- hospital_app/src/components/DenseTable.js
+++ hospital_app/src/components/DenseTable.js
@@ -12,33 +12,13 @@ function createData(patient_id, firstname, lastname, DOB, admitted) {
   return { patient_id, firstname, lastname, DOB, admitted };
 }
 
-
-
-// const rows = [
-//   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-//   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-//   createData('Eclair', 262, 16.0, 24, 6.0),
-//   createData('Cupcake', 305, 3.7, 67, 4.3),
-//   createData('Gingerbread', 356, 16.0, 49, 3.9),
-// ];
-
 export default function DenseTable() {
-  // const[patients, setPatients] = useState([]);
-
-  // useEffect(()=>{
-  //   fetch("http://localhost:8080/patient/patient_id/allpatients")
-  //   .then(res=>res.json())
-  //   .then((result)=>{
-  //     setPatients(result);
-  //   }
-  // )
-  // }, [])
 
   const[patients, setPatients] = useState([]);
   const rows= [];
 
   useEffect(()=>{
-    fetch("http://localhost:8080/patient/patient_id/allpatients")
+    fetch("http://localhost:8080/patient/patient_id/bynurseid")
     .then(res=>res.json())
     .then((result)=>{
       setPatients(result);
